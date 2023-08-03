fbcb796f7623ed8b5ce6000161bd3e8130d5ba13
TaichiN27 <ragu.cap@icloud.com>
2023-08-03 14:54:03 +0900

    feat: パスワードハッシュ化

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 20230803005124) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
