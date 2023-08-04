3ed59eb89be294e61fa172b50645639d442eaff2
TaichiN27 <ragu.cap@icloud.com>
2023-08-04 13:25:39 +0900

    feat: 編集フォームと更新処理追加しました

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
     #debugger
@@ -17,6 +18,20 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
