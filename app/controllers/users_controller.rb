60d276ea728e3fa41fbded67eaf68bc7122a3f9f
TaichiN27 <ragu.cap@icloud.com>
2023-08-04 13:28:26 +0900

    refactor: ７章プルリクエクの時指摘していただいたコメントアウト関連消去しました

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
-    #debugger
+
 
   end
 
@@ -41,4 +41,6 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+
+    
 end
