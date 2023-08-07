42102e61717fca10bf00706963bc31265ca84893
TaichiN27 <ragu.cap@icloud.com>
2023-08-07 15:17:31 +0900

    refactor: ログイン後ルートの修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       if user.activated?
+        # Success
         log_in user
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
         redirect_back_or user
@@ -14,10 +15,8 @@ class SessionsController < ApplicationController
         flash[:warning] = message
         redirect_to root_url
       end
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or root_url
     else
+      # Failure
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
