028c105faae1f1569604a68edff0c6a56c5394de
TaichiN27 <ragu.cap@icloud.com>
2023-08-09 14:08:37 +0900

    feat: セッションコントローラーにログイン時ユーザーが有効化されているかの機能を追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       if user.activated?
+        # Success
         log_in user
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
         redirect_back_or user
@@ -14,16 +15,14 @@ class SessionsController < ApplicationController
         flash[:warning] = message
         redirect_to root_url
       end
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or root_url
     else
+      # Failure
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
   def destroy
-    log_out if logged_in?
+    log_out
     redirect_to root_url
   end
 end
